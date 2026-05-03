f446554815b074c3bf9d238ff731e63579e7d30d
Marcel Ulbrich <marcel@ulbrich.media>
2026-05-03 21:49:35 +0200

    fix: profile picture handling fixed

--- api/src/Http/Resources/AnimalListResource.php
+++ api/src/Http/Resources/AnimalListResource.php
@@ -10,13 +10,12 @@ class AnimalListResource extends AnimalBaseResource
     public function toArray(Request $request): array
     {
         return array_merge(parent::toArray($request), [
-            'profile_picture_url' => $this->resource->when(
-                $this->resource->relationLoaded('media'),
-                fn () => $this->resource->getMedia('pictures')
+            'profile_picture_url' => $this->resource->relationLoaded('media')
+                ? $this->resource->getMedia('pictures')
                     ->sortBy('order_column')
                     ->first(fn (Media $media) => str_starts_with($media->mime_type ?? '', 'image/'))
-                    ?->getTemporaryUrl(now()->addHour(), 'thumbnail'),
-            ),
+                    ?->getTemporaryUrl(now()->addHour(), 'thumbnail')
+                : null,
         ]);
     }
 }
